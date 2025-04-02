aaa53263e38fbffe905dc36df0f12ce6254ca984
shima8823 <taiyaki1275@gmail.com>
2025-04-02 23:14:09 +0900

    initial commit

---
+++ ex01/main.c
@@ -0,0 +1,12 @@
+#include <linux/module.h> /* Needed by all modules */ 
+#include <linux/printk.h> /* Needed for pr_info() */ 
+  
+int init_module(void) {
+	pr_info("Hello world !\n");
+	return 0; 
+}
+
+void cleanup_module(void) { 
+	pr_info("Cleaning up module.\n"); 
+}
+MODULE_LICENSE("GPL");
