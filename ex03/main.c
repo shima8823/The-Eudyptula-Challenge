4f75bc4387d8fc8b6d1011948bdf739861b9e471
shima8823 <taiyaki1275@gmail.com>
2025-11-14 00:08:39 +0900

    Remove unnecessary comment in do_work function to enhance code clarity

--- ex03/main.c
+++ ex03/main.c
@@ -13,7 +13,6 @@ int do_work(int *my_int, int retval)
 				udelay(10);
 
 		if (y < 10)
-				/* That was a long sleep, tell userspace about it */
 				pr_info("We slept a long time!");
 
 		z = x * y;
