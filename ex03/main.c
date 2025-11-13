9a31a620f292bf8e80fce039f9e63a27ac9c565b
shima8823 <taiyaki1275@gmail.com>
2025-11-13 23:47:10 +0900

    Refactor indent's space to 8 space tab

--- ex03/main.c
+++ ex03/main.c
@@ -5,29 +5,28 @@
 
 int do_work(int *my_int, int retval)
 {
-        int x;
-        int y = *my_int;
-        int z;
+		int x;
+		int y = *my_int;
+		int z;
 
-        for (x = 0; x < my_int; ++x) {
-                udelay(10);
-        }
+		for (x = 0; x < my_int; ++x)
+				udelay(10);
 
-        if (y < 10)
-                /* That was a long sleep, tell userspace about it */
-                pr_info("We slept a long time!");
+		if (y < 10)
+				/* That was a long sleep, tell userspace about it */
+				pr_info("We slept a long time!");
 
-        z = x * y;
-        return z;
-            return 1;
+		z = x * y;
+		return z;
+		return 1;
 }
 
 int my_init(void)
 {
-        int x = 10;
+		int x = 10;
 
-        x = do_work(&x, x);
-        return x;
+		x = do_work(&x, x);
+		return x;
 }
 
 void my_exit(void)
