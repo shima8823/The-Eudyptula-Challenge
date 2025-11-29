8950cfd553b5a8e0c56b1dc4aed87a402052ec70
shima8823 <taiyaki1275@gmail.com>
2025-11-29 17:48:34 +0900

    Add debugfs support in main.c to create a directory and files for debugging purposes, including write operations and cleanup on module exit.

--- ex07/main.c
+++ ex07/main.c
@@ -1,16 +1,35 @@
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/debugfs.h>
 
 MODULE_LICENSE("GPL");
 
-static int __init hello_init(void) {
-	pr_info("Hello world !\n");
+static struct dentry *dir = NULL;
+
+static int add_write_op(void *data, u64 value)
+{
+	return 0;
+}
+DEFINE_SIMPLE_ATTRIBUTE(add_fops, NULL, add_write_op, "%llu\n");
+
+
+static int __init debugfs_init(void) {
+	dir = debugfs_create_dir("fortytwo", NULL);
+
+	// assinment 5
+	debugfs_create_file("id", 0666, dir, NULL, &add_fops);
+	// 
+	debugfs_create_file("jiffies", 0444, dir, NULL, &add_fops);
+	// 
+	debugfs_create_file("foo", 0644, dir, NULL, &add_fops);
+
+	// debugfs_create_u32("sum", 0444, dir, &sum);
 	return 0;
 }
 
-static void __exit hello_exit(void) { 
-	pr_info("Cleaning up module.\n");
+static void __exit debugfs_exit(void) { 
+	debugfs_remove_recursive(dir);
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(debugfs_init);
+module_exit(debugfs_exit);
