b9d968617677dcfd2ab406042f5853bbf851b68d
shima8823 <taiyaki1275@gmail.com>
2025-12-02 01:51:14 +0900

    Change file_operations structure to const in main.c for improved safety and clarity.

--- ex08/main.c
+++ ex08/main.c
@@ -14,7 +14,7 @@ static ssize_t myfd_read(struct file *fp, char __user *user,
 static ssize_t myfd_write(struct file *fp, const char __user *user,
 			  size_t size, loff_t *offs);
 
-static struct file_operations myfd_fops = {
+static const struct file_operations myfd_fops = {
 	.owner = THIS_MODULE,
 	.read = &myfd_read,
 	.write = &myfd_write
