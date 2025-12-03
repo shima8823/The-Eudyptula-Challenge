4326b7333aa5aeb3de77b025da02cbb418bc9838
shima8823 <taiyaki1275@gmail.com>
2025-12-03 18:12:27 +0900

    Enhance mount iteration in main.c by adding root path checks and improving output formatting. Allocate and free memory for mount point paths to prevent memory leaks.

--- ex09/main.c
+++ ex09/main.c
@@ -1,24 +1,45 @@
 #include <linux/module.h>
 #include <linux/printk.h>
-#include <linux/namei.h>
 #include <linux/dcache.h>
-#include <linux/list.h>
+#include <linux/fs_struct.h>
 #include <linux/nsproxy.h>
 #include <linux/rbtree.h>
+#include <linux/slab.h>
 #include <../fs/mount.h>
 
 MODULE_LICENSE("GPL");
 
 static int __init misc_init(void)
 {
-	struct mount *mnt;
 	struct mnt_namespace *ns = current->nsproxy->mnt_ns;
-
-	for (struct rb_node *node = ns->mnt_first_node; node; node = rb_next(node)) {
-		mnt = rb_entry(node, struct mount, mnt_node);
-		pr_info("my_mount: %s\n", mnt->mnt_devname);
-	}
+	char *buf;
+	struct path root;
 	
+	buf = kmalloc(PATH_MAX, GFP_KERNEL);
+	if (!buf)
+		return -ENOMEM;
+
+	get_fs_root(current->fs, &root);
+	for (struct rb_node *node = ns->mnt_first_node; node; node = rb_next(node)) {
+		struct mount *mnt = rb_entry(node, struct mount, mnt_node);
+		const struct path mnt_path = {
+			.mnt = &mnt->mnt,
+			.dentry = mnt->mnt.mnt_root
+		};
+
+		// skip rootfs
+		if (!path_is_under(&mnt_path, &root))
+			continue;
+		char *mount_point = dentry_path_raw(mnt->mnt_mountpoint, buf, PATH_MAX);
+		if (IS_ERR(mount_point))
+			continue;
+		const char *name = mnt->mnt_mountpoint->d_name.name;
+		if (strcmp(name, "/") == 0)
+			name = "root";
+		pr_info("%s\t%s\n", name, mount_point);
+	}
+	path_put(&root);
+	kfree(buf);
 	return 0;
 }
 
